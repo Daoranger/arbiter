f788b11a975393f4c9ac7e721a36fde5562e0c15
Hoang Nguyen <uchiha.kamui2004@gmail.com>
2026-09-02 12:39:33 -0400

    Added Entity

---
+++ include/ECS/Entity.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Entity
+{
+public:
+    float currentHealth = 100.0f, maxHealth = 100.0f;
+    float currentEnergy = 100.0f, maxEnergy = 100.0f;
+    float currentHunger = 100.0f, maxHunger = 100.0f;
+    float currentThirst = 100.0f, maxThirst = 100.0f;
+    float bodyTemp      = 37.0f;
+
+    float medCount = 0.0f;
+    float foodCount = 0.0f;
+    float waterCount = 0.0f;
+};
+
+
+
